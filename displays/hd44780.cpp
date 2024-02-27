bac2a9cb46236d22e67a8f09527f61a020b9f656
Chris <chris@koolecontrols.nl>
2024-02-27 16:46:26 +0100

    Start on HD44870 display driver

---
+++ displays/hd44780.cpp
@@ -0,0 +1,206 @@
+#include "hd44780.h"
+#include "kernel.h"
+
+#define LCD_DATA_PINS	(Pins::DB0 | Pins::DB1 | Pins::DB2 | Pins::DB3 | Pins::DB4 | Pins::DB5 | Pins::DB6 | Pins::DB7)
+#define LCD_ALL_PINS	(Pins::RS | Pins::RW | Pins::E | LCD_DATA_PINS | Pins::BL)
+
+#define HD44780_INIT_SEQ        0x30
+#define HD44780_DISP_CLEAR      0x01
+#define HD44780_DISP_OFF        0x08
+#define HD44780_DISP_ON         0x0C
+#define HD44780_CURSOR_ON       0x0E
+#define HD44780_CURSOR_OFF      0x0C
+#define HD44780_CURSOR_BLINK    0x0F
+#define HD44780_RETURN_HOME     0x02 
+#define HD44780_ENTRY_MODE      0x06
+#define HD44780_4BIT_MODE       0x20
+#define HD44780_8BIT_MODE       0x30
+#define HD44780_2_ROWS          0x08
+#define HD44780_FONT_5x8        0x00
+#define HD44780_FONT_5x10       0x04
+#define HD44780_POSITION        0x80
+
+#define HD44780_SHIFT           0x10
+#define HD44780_CURSOR          0x00
+#define HD44780_DISPLAY         0x08
+#define HD44780_LEFT            0x00
+#define HD44780_RIGHT           0x04
+
+#define HD44780_ROWS            2
+#define HD44780_COLS            16
+
+#define HD44780_ROW1_START      0x00
+#define HD44780_ROW1_END        HD44780_COLS
+#define HD44780_ROW2_START      0x40
+#define HD44780_ROW2_END        HD44780_COLS
+
+Result HD44780::DeviceSetConfig(IDeviceConfig &config)
+{
+	const char* temp;
+    ContextLock lock(mutex);
+	RETURN_ON_ERR(config.getProperty("gpioDevice", &gpioDeviceKey));
+	RETURN_ON_ERR(config.getProperty("pin_D0", temp));
+
+	 if(sscanf(temp, "%hhu,%hhu", &D0_Port, &D0_pin) != 2) return Result::Error;
+
+
+
+	DeviceSetStatus(DeviceStatus::Dependencies);
+	return Result::Ok;
+}
+
+Result HD44780::DeviceLoadDependencies(std::shared_ptr<DeviceManager> deviceManager)
+{
+	ContextLock lock(mutex);
+	RETURN_ON_ERR(deviceManager->getDeviceByKey<IGpio>(gpioDeviceKey, gpioDevice));
+	gpioDevice->gpioWrite(D_0port, )
+	DeviceSetStatus(DeviceStatus::Initializing);
+	return Result::Ok;
+}
+
+HD44780::Pins FromData(uint8_t data)
+{
+	return (HD44780::Pins)data;
+}
+
+Result HD44780::DeviceInit()
+{
+	ContextLock lock(mutex);
+    RETURN_ON_ERR(rwPin.DeviceInit());
+    RETURN_ON_ERR(rsPin.DeviceInit());
+    RETURN_ON_ERR(ePin.DeviceInit());
+	RETURN_ON_ERR(blPin.DeviceInit());
+
+	//Initialize non-SPI GPIOs
+    RETURN_ON_ERR(rwPin.GpioConfigure(&gpioOutput));
+    RETURN_ON_ERR(rsPin.GpioConfigure(&gpioOutput));
+    RETURN_ON_ERR(ePin.GpioConfigure(&gpioOutput));
+	RETURN_ON_ERR(blPin.GpioConfigure(&gpioOutput));
+
+	RETURN_ON_ERR(rwPin.GpioPinWrite(0));
+	RETURN_ON_ERR(rsPin.GpioPinWrite(0));
+	RETURN_ON_ERR(ePin.GpioPinWrite(0));
+	RETURN_ON_ERR(blPin.GpioPinWrite(0));
+	
+
+    //Turn on backlight
+    RETURN_ON_ERR(blPin.GpioPinWrite(1));   
+
+	ESP_LOGI(TAG, "Initializing");
+	io.SetPinsMode(LCD_ALL_PINS, PinModes::PIN_OUTPUT);
+	io.SetPins(LCD_ALL_PINS, Pins::NONE);				//All pins low
+	vTaskDelay(pdMS_TO_TICKS(100));								//Wait for screen
+
+	io.SetPins(LCD_DATA_PINS, FromData(0xFF));
+	vTaskDelay(pdMS_TO_TICKS(10));	
+	
+//	io.SetPins(Pins::E | Pins::RW | Pins::RS, Pins::E);
+	RETURN_ON_ERR(ePin.GpioPinWrite(1));
+	io.SetPins(LCD_ALL_PINS, Pins::NONE);				//All pins low
+	vTaskDelay(pdMS_TO_TICKS(10));	
+//	io.SetPins(Pins::E | Pins::RW | Pins::RS, Pins::NONE);
+	RETURN_ON_ERR(ePin.GpioPinWrite(0));
+	vTaskDelay(pdMS_TO_TICKS(10));
+		
+	vTaskDelay(pdMS_TO_TICKS(1000));	
+		
+	vTaskDelay(pdMS_TO_TICKS(10));	
+	
+	vTaskDelay(pdMS_TO_TICKS(10));	
+	io.SetPins(Pins::E | Pins::RW | Pins::RS, FromData(0x30));
+
+	vTaskDelay(pdMS_TO_TICKS(10));	
+	io.SetPins(LCD_DATA_PINS, FromData(0x30) | Pins::E);
+	
+	
+	
+	
+	LCD_cmd(HD44780_INIT_SEQ);
+	vTaskDelay(pdMS_TO_TICKS(105)); //+5	 
+	//ets_delay_us(4500);
+	
+	LCD_cmd(HD44780_INIT_SEQ);
+	vTaskDelay(pdMS_TO_TICKS(101));	//+1
+	//ets_delay_us(120);
+	
+	LCD_cmd(HD44780_INIT_SEQ);
+	vTaskDelay(pdMS_TO_TICKS(101));	//+1
+	//ets_delay_us(50);
+	
+	LCD_cmd(HD44780_8BIT_MODE | HD44780_2_ROWS | HD44780_FONT_5x10);
+	LCD_cmd(HD44780_DISP_OFF);
+	LCD_cmd(HD44780_DISP_CLEAR);
+	LCD_cmd(HD44780_ENTRY_MODE | HD44780_RETURN_HOME);
+	LCD_cmd(HD44780_DISP_ON);
+	LCD_cmd(HD44780_CURSOR_OFF);
+	
+	
+	
+	//WaitBFClear();
+}
+
+
+
+
+
+void HD44780::SetBacklight(bool enabled)
+{
+	if (enabled)
+		io.SetPins(Pins::BL, Pins::BL);
+	else		
+		io.SetPins(Pins::BL, Pins::NONE);
+	
+}
+
+
+void HD44780::SetCursor(int x, int row)
+{
+	x %= 16;
+	row %= 2;
+	if (row == 1) x += 64;
+	LCD_cmd(HD44780_POSITION + x);
+}
+
+
+void HD44780::LCD_cmd(unsigned char cmd)
+{
+	io.SetPins(Pins::E | Pins::RW | Pins::RS, Pins::E);
+	vTaskDelay(pdMS_TO_TICKS(10));	
+	io.SetPins(LCD_DATA_PINS, FromData(cmd));
+	vTaskDelay(pdMS_TO_TICKS(10));	
+	io.SetPins(Pins::E | Pins::RW | Pins::RS, Pins::NONE);
+}
+
+
+void HD44780::WaitBFClear()
+{
+	vTaskDelay(pdMS_TO_TICKS(1));
+	//ets_delay_us(100);	//TODO
+}
+
+
+void HD44780::LCD_Data(unsigned char cmd)
+{
+	io.SetPins(Pins::E | Pins::RW | Pins::RS, Pins::RS | Pins::E);
+	io.SetPins(LCD_DATA_PINS, FromData(cmd));
+	vTaskDelay(pdMS_TO_TICKS(1));	//TODO Whaaaa?? This HAS to be �S...
+	//ets_delay_us(1);
+	io.SetPins(Pins::E | Pins::RW | Pins::RS, Pins::NONE);
+	WaitBFClear();
+}
+
+
+void HD44780::Write(std::string message, int x, int y)
+{
+	SetCursor(x, y);
+	Write(message);
+}
+
+
+void HD44780::Write(std::string message)
+{
+	for (int i = 0; i < message.length(); i++)
+	{
+		LCD_Data(message.c_str()[i]);
+	}
+}
---
+++ displays/hd44780.h
@@ -0,0 +1,57 @@
+#pragma once
+#include "esp_system.h"
+#include "mcp23s17.h"
+#include <string>
+#include "gpio.h"
+//https://github.com/Matiasus/HD44780/blob/master/lib/hd44780.c
+
+
+class HD44780
+{
+public:
+	enum class Pins
+	{
+		NONE = 0x0000,
+		RW   = 0x0001,
+		RS   = 0x0002,
+		E    = 0x0004,
+		BL   = 0x0008,
+		DB0  = 0x0010,
+		DB1  = 0x0020,
+		DB2  = 0x0040,
+		DB3  = 0x0080,
+		DB4  = 0x0100,
+		DB5  = 0x0200,
+		DB6  = 0x0400,
+		DB7  = 0x0800,
+		ALL  = 0x0FFF,
+	};
+
+	
+	class IGPIO
+	{
+	public:
+		virtual void SetPinsMode(Pins mask, PinModes mode)	= 0;
+		virtual void SetPins(Pins mask, Pins value)	= 0;
+		virtual void GetPins(Pins mask, Pins* value) = 0;
+	};
+	
+private:
+	IGPIO& io;
+	const char* TAG = "HD44780";
+	void SetCursor(int x, int row);
+	void LCD_cmd(unsigned char cmd);
+	void WaitBFClear();
+	void LCD_Data(unsigned char cmd);
+		
+public:
+	HD44780(IGPIO& io);
+	void SetBacklight(bool enabled);
+	void Write(std::string message);
+	void Write(std::string message, int x, int y);
+};
+
+
+DEFINE_ENUM_CLASS_FLAG_OPERATORS(HD44780::Pins, uint32_t);
+
+
