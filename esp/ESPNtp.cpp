a9b5bef3e0222d060968c17b8013823494fe703d
Chris <chris@koolecontrols.nl>
2024-04-05 13:59:30 +0200

    try to merge

--- esp/ESPNtp.cpp
+++ esp/ESPNtp.cpp
@@ -3,8 +3,12 @@
 #include <sys/time.h>
 #include "esp_sntp.h"
 
+ESPNtp::ESPNtp(std::shared_ptr<DeviceManager> deviceManager) : deviceManager(deviceManager)
+{
+    ContextLock lock(mutex);
+}
 
-std::function<void(DateTime newTime)> ESPNtp::callback;
+//std::function<void(DateTime newTime)> ESPNtp::callback;
 
 Result ESPNtp::DeviceSetConfig(IDeviceConfig &config)
 {
@@ -14,7 +18,7 @@ Result ESPNtp::DeviceSetConfig(IDeviceConfig &config)
 	return Result::Ok;
 }
 
-Result ESPNtp::DeviceLoadDependencies(std::shared_ptr<DeviceManager> deviceManager)
+Result ESPNtp::DeviceLoadDependencies(std::shared_ptr<DeviceManager> deviceManager) 
 {
 	ContextLock lock(mutex);
 	DeviceSetStatus(DeviceStatus::Initializing);
@@ -29,20 +33,28 @@ void ESPNtp::timeSyncCallback(timeval *tv)
     }
 }
 
-
 Result ESPNtp::DeviceInit()
 {
 	ContextLock lock(mutex);
     Endpoint ep;
     ip_addr_t ip;
     DNSResolver resolver;
-    RETURN_ON_ERR(resolver.Resolve(server, ep));
-    RETURN_ON_ERR(ep.GetIP(&ip));
-    sntp_init();
-    sntp_setserver(0, &ip);
-    sntp_set_time_sync_notification_cb(timeSyncCallback);
-	DeviceSetStatus(DeviceStatus::Ready);
-	return Result::Ok;
+    // check if wifi or ehternet is ready
+    std::shared_ptr<ESPWifi> wifi;
+    std::shared_ptr<LAN87xx> lan;
+    if( (deviceManager->getDeviceByCompatibility("esp_wifi", wifi) == Result::Ok) ||
+        (deviceManager->getDeviceByCompatibility("lan87xx", lan) == Result::Ok) )
+    {
+        RETURN_ON_ERR(resolver.Resolve(server, ep));
+        RETURN_ON_ERR(ep.GetIP(&ip));
+        sntp_init();
+        sntp_setserver(0, &ip);
+        sntp_set_time_sync_notification_cb(timeSyncCallback);
+        DeviceSetStatus(DeviceStatus::Ready);
+        return Result::Ok;
+    }
+    else  DeviceSetStatus(DeviceStatus::Dependencies);
+     return Result::Ok;
 }
 
 Result ESPNtp::NtpOnSyncSetCallback(std::function<void(DateTime newTime)> callback)
--- esp/ESPNtp.h
+++ esp/ESPNtp.h
@@ -4,12 +4,23 @@
 #include "INtp.h"
 #include "esp_sntp.h"
 #include <sys/time.h>
-
+#include "devicemanagement.h"
+#include "ESPWifi.h"
+#include "lan87xx.h"
 
 class ESPNtp : public INtp
 {
     constexpr static const char* TAG = "ESPNtp";
 	Mutex mutex;
+
+    // Dependencies:
+    // must wait for ethernet of wifi initalized
+
+
+    std::shared_ptr<ESPWifi> wifi;
+    std::shared_ptr<LAN87xx> lan;
+    std::shared_ptr<DeviceManager> deviceManager;
+
     static std::function<void(DateTime newTime)> callback;
 
     static void timeSyncCallback(struct timeval *tv);
@@ -20,6 +31,7 @@ class ESPNtp : public INtp
 
 public:
 
+    ESPNtp(std::shared_ptr<DeviceManager> deviceManager);
 	virtual Result DeviceSetConfig(IDeviceConfig& config) override;
     virtual Result DeviceLoadDependencies(std::shared_ptr<DeviceManager> deviceManager) override;
     virtual Result DeviceInit() override;
